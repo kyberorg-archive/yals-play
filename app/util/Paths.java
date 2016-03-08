976286a2f958d3ea140c265489d5295fd79c0aa0
Alex Muravya <asm@virtalab.net>
2016-03-08 05:34:47 +0200

    Renaming project

--- app/util/Paths.java
+++ app/util/Paths.java
@@ -9,7 +9,7 @@ public class Paths {
     public static final String API_STORE = "/api/store";
     public static final String API_GET_LINK = "/api/link/";
 
-    public static final String OUR_GITHUB_REPO = "https://github.com/virtalab/hz.am";
+    public static final String OUR_GITHUB_REPO = "https://github.com/virtalab/yals";
     public static final String GITHUB_COMMIT_PATH = "/commit/";
 
     public static final String TEST_CSS = "/css/test.css";
